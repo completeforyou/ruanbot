add1e0ea61663673c6c8e20f058b26fff9ebab43
completeforyou <completeforyou@pm.me>
2026-02-21 19:47:25 -0600

    leaderboard typo

--- handlers/leaderboard.py
+++ handlers/leaderboard.py
@@ -55,7 +55,7 @@ async def render_leaderboard(update: Update, page: int, sort_by: str, is_new: bo
         return
     
     # 4. Build Text
-    title = "🏆 积分排行榜" if sort_by == 'points' else "🗣 活跃榜"
+    title = "🏆 积分排行榜" if sort_by == 'points' else "🗣 日活跃榜"
     text = f"<b>{title} (Top {MAX_ITEMS})</b>\n"
     text += "━━━━━━━━━━━━━━\n"
     
